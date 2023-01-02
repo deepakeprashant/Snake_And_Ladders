59a528941adb119b0554da3287b8f76dc4072ba5
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-02 21:49:03 +0530

    ADDED: COUNT THE NUMBER OF THROW DICE

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -7,11 +7,13 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("WELCOME TO SNAKE LADDERS");
         int firstPlayerPosition = 0;
+        int counterRolling = 0;
         System.out.println("PLAYER STARTING POSITION " + firstPlayerPosition);
         while (firstPlayerPosition < WINNING_POSITION) {
             int numberAfterRolling = (int) Math.floor(Math.random() * 10) % 6 + 1;
             System.out.println("Number after rolling the dice by the player: " + numberAfterRolling);
             int checkOptions = (int) Math.floor(Math.random() * 10) % 3;
+            ++counterRolling;
             switch (checkOptions) {
                 case NO_PLAY:
                     System.out.println("Player is not playing therefore stays in the same position");
@@ -30,5 +32,6 @@ public class SnakeLadder {
                     System.out.println("Player got snake bite then position is: " + firstPlayerPosition);
             }
         }
+        System.out.println("\n:: Number of times dice was thrown by the players: " + counterRolling);
     }
 }
