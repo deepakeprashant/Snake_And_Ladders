aa0f5d6e61a4c1dd601598af7560c6c53f9166b6
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-02 20:29:31 +0530

    ADDED: PLAYER STARTING POSITION

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,5 +1,7 @@
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("WELCOME TO SNAKE LADDERS");
+        int firstPlayerPosition = 0;
+        System.out.println("PLAYER STARTING POSITION "+firstPlayerPosition);
     }
 }
