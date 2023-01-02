d6bb985500e25755fd99632653e6c607260f5180
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-02 20:50:04 +0530

    ADDED:PLAYER ROLLING DIE

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -3,5 +3,7 @@ public class SnakeLadder {
         System.out.println("WELCOME TO SNAKE LADDERS");
         int firstPlayerPosition = 0;
         System.out.println("PLAYER STARTING POSITION "+firstPlayerPosition);
+        int numberAfterRolling = (int) Math.floor(Math.random() * 10) % 6 +1;
+        System.out.println("Number after rolling the dice by the player: " + numberAfterRolling);
     }
 }
